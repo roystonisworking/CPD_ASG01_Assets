60cfaed88bdf7fb8d6f7f54ad0e4124f39ab51e5
RandoM3L <41134699+RandoM3L@users.noreply.github.com>
2018-07-12 11:46:58 +0800

    Add files via upload

---
+++ uiRaceTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiRaceTimer : MonoBehaviour {
+
+	public Text timerText;
+
+	public float seconds; 
+	public float minutes;
+
+
+	// Use this for initialization
+	void Start () {
+
+		timerText = GetComponent<Text> () as Text;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		minutes = (int)(Time.timeSinceLevelLoad / 60f);
+		seconds = (int)(Time.timeSinceLevelLoad % 60f);
+		timerText.text = "Timer : " + minutes.ToString("00") + ":" + seconds.ToString("00");
+	}
+}
